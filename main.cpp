cc0e5062ec2cc868de8a8086840dbe2763602290
Ultrasubmarine <sybmarina3000@yandex.ru>
2023-11-27 20:47:15 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  EPet
+//
+//  Created by marina porkhunova on 27.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "---Hello--\n";
+    
+    
+    return 0;
+}
